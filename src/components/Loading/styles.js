117c390e8efcc381ed4a0f3a79dc2300fc48d2b5
vtorres96 <victorcdc96@gmail.com>
2020-12-30 07:23:29 -0300

    added components styles: Loading

---
+++ src/components/Loading/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: fixed;
+  top:0;
+  left:0;
+  right: 0;
+  bottom: 0;
+  z-index: 99;
+  background-color: #000;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+`;
