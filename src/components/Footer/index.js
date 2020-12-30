311265f1d4ba565d893e3ba1a2e9ea50b41fb6e1
vtorres96 <victorcdc96@gmail.com>
2020-12-30 07:21:39 -0300

    added components: Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Footer = () => {
+  return (
+    <Container>
+      Desenvolvido por Vtorres96 <br/>
+      Todos os direitos das imagens são da Netflix <br/>
+      Dados Extraidos de https://www.themoviedb.org/ <br/>
+    </Container>
+  );
+};
+
+export default Footer;
