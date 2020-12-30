ad844f9088798bd8a5ee0ff1393b82c07a32015c
vtorres96 <victorcdc96@gmail.com>
2020-12-30 07:21:52 -0300

    added components: Loading

---
+++ src/components/Loading/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Loading = () => {
+  return (
+    <Container>
+      <img src="https://cdn.lowgif.com/small/0534e2a412eeb281-the-counterintuitive-tech-behind-netflix-s-worldwide.gif" alt="loading"></img>
+    </Container>
+  );
+};
+
+export default Loading;
